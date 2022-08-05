975d6a8454c0df7482824ab2c844df7447ad6a76
Hugo Dugalleix <hugodugalleixiribarrengmail.com>
2022-08-05 22:57:37 +0200

    frontv3

--- front/components/Footer.js
+++ front/components/Footer.js
@@ -5,8 +5,9 @@ import React from "react";
 
 function Footer() {
     return (
-      <div>
-        <img className="p-4 h-2/3 object-center " rel="icon" type="png" href="Expliimg.png" src= '/Alyralogo.png'  alt="explimg"></img>
+      <div className="flex">
+        <img className="p-4 h-2/3 object-center flex-start" rel="icon" type="png" src= '/Alyralogo.png'  alt=""></img>
+        <img className="p-4 h-2/3 object-center flex-end" rel="icon" type="png" src= '/Copyright.png'  alt=""></img>
       </div>
     );
   }
